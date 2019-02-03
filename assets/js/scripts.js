40b109f1a59dbb7361f3e661c18a4e60ae694d36
Gimka <42877856+Gimsky@users.noreply.github.com>
2019-02-04 01:47:43 +0300

    Upgrade form registration

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,13 @@ $( document ).ready(function () {
     $(this).siblings("form").addClass("visible");
     $(this).hide();
   });
+  $('#registerLink').click(function(){
+    $("#enterFormBlock").removeClass('perspectiveLeftReturn');
+    $("#enterFormBlock").addClass('magictime perspectiveLeft');
+    $('#backSideBlock').fadeIn(600);});
+
+  $('#enterLink').click(function(){
+    $("#enterFormBlock").removeClass('perspectiveLeft');
+    $("#enterFormBlock").addClass('magictime perspectiveLeftReturn');
+    $('#backSideBlock').fadeOut(600);});
 })
